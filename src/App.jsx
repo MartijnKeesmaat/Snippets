fca59274357532f57ec15be0c23dbd72bcbf6ea7
Martijn Keesmaat <martijn@bit-students.com>
2018-04-18 20:36:49 +0200

    React setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from 'components/Header.jsx';
+// import './App.css';
+
+class App extends Component {
+  state = {
+    snippets: {}
+  };
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends React.component {
+  render() {
+    return (
+      <header>
+        <a href="#">Log in</a>
+        <a href="#">Log out</a>
+        <a href="#">Sign up</a>
+      </header>
+    );
+  }
+}
