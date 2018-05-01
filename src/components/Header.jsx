98df96b53c014807028a7e1dc4b7d1403d10022d
Martijn Keesmaat <martijn@bit-students.com>
2018-05-01 18:56:56 +0200

    Search with dummy data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,14 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <a href="">Log in</a>
-        <a href="">Log out</a>
-        <a href="">Sign up</a>
+        <a href="">Filter</a>
+        <input type="text" placeholder="filter this.." />
+
+        <div className="account">
+          <a href="">Log in</a>
+          <a href="">Log out</a>
+          <a href="">Sign up</a>
+        </div>
       </header>
     );
   }
--- src/components/Snippets.jsx
+++ src/components/Snippets.jsx
@@ -2,16 +2,55 @@ import React from 'react';
 import Snippet from './Snippet';
 
 class Snippets extends React.Component {
+  state = {
+    snippets: [],
+    initialSnippets: [
+      {
+        dateCreated: '29/04/2018',
+        description:
+          'There is a moment in the life of any aspiring astr… think about setting up your own viewing station.',
+        favorite: false,
+        files: Array(2),
+        timeCreated: '15:10:29',
+        title: 'abc'
+      },
+      {
+        dateCreated: '29/04/2018',
+        description:
+          'There is a moment in the life of any aspiring astr… think about setting up your own viewing station.',
+        favorite: false,
+        files: Array(2),
+        timeCreated: '15:10:29',
+        title: 'def'
+      }
+    ]
+  };
+
+  filterList = e => {
+    let updatedList = this.state.initialSnippets;
+    updatedList = updatedList.filter(snippet => {
+      return (
+        snippet.title.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+      );
+    });
+    this.setState({ snippets: updatedList });
+  };
+
+  componentWillMount() {
+    this.setState({ snippets: this.state.initialSnippets });
+  }
+
   render() {
     return (
       <div className="main-content__inner main-content__inner--snippets">
         <div className="scroll-container">
+          <input type="text" onChange={this.filterList} />
           {this.props.snippets.length > 0 &&
-            this.props.snippets.map((snippet, key) => (
+            this.state.snippets.map((snippet, key) => (
               <Snippet
                 key={key}
                 index={key}
-                details={this.props.snippets[key]}
+                details={this.state.snippets[key]}
                 showSnippetDetail={this.props.showSnippetDetail}
               />
             ))}
